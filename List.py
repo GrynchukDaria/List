470bcd4ea24223095b9337d25fe8b4b756528aaa
Daria Grynchuk <70750118+GrynchukDaria@users.noreply.github.com>
2020-09-10 16:10:28 +0300

    Initial commit

---
+++ List.py
@@ -0,0 +1,10 @@
+shoppinglist = ["milk", "bread", "yoghurt", "salad"]
+print(shoppinglist)
+add = "tea"
+shoppinglist.insert(3,"tea")
+print(shoppinglist)
+shoppinglist.remove("salad")
+print(shoppinglist)
+
+
+
